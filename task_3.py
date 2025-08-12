c85ee6817cd8355973e7e45e8d27da09da6d38a2
Mihaylovskaya <chillgirl_88@outlook.com>
2025-08-12 16:49:11 +0300

    added some information task_1,task_2, task_3, task_7,

--- task_3.py
+++ task_3.py
@@ -1,3 +1,30 @@
+""" Задача 3. JSON-логирование
+Что нужно сделать
+Удобно сохранять логи в определённом формате, чтобы затем их можно было фильтровать и анализировать. Сконфигурируйте логгер так, чтобы он писал логи в файл skillbox_json_messages.log в следующем формате:
+
+{"time": "<время>", "level": "<уровень лога>", "message": "<сообщение>"}
+Но есть проблема: если в message передать двойную кавычку, то лог перестанет быть валидной JSON-строкой:
+
+{"time": "21:54:15", "level": "INFO", "message": }
+Чтобы этого избежать, потребуется LoggerAdapter. Это класс из модуля logging, который позволяет модифицировать логи перед тем, как они выводятся.
+
+У него есть единственный метод — process, который изменяет сообщение или именованные аргументы, переданные на вход.
+
+class JsonAdapter(logging.LoggerAdapter):
+def process(self, msg, kwargs):
+# меняем msg
+return msg, kwargs
+Использовать можно так:
+
+logger = JsonAdapter(logging.getLogger(__name__))
+logger.info('Сообщение')
+Вам нужно дописать метод process так, чтобы в логах была всегда JSON-валидная строка.
+
+Советы и рекомендации
+LoggerAdapter Objects.
+Обратите внимание: кавычки нужно экранировать, а не удалять или заменять на другие. Помимо кавычек есть и другие тонкости, например переносы строк. Чтобы всё учесть, воспользуйтесь методом json.dumps.
+ """
+
 import logging
 import json
 from datetime import datetime
@@ -19,10 +46,10 @@ logging.basicConfig(
     level=logging.INFO,
     filename='skillbox_json_messages.log',
     filemode='a',
-    encoding='utf-8',
+    encoding='Windows-1251',
     format='%(message)s'
 )
 
 logger = JsonAdapter(logging.getLogger(__name__), {})
 
-logger.info('Сообщение с "кавычками" и переносом \nстроки')
+logger.info('Message with "quotes" and \nline breaks')
